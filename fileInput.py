96b6c72dc1ff4ffaed60e8a5604da4c9af76c7c5
Abhinav Kompella <101344801+kompellabhinav@users.noreply.github.com>
2022-12-07 20:11:18 -0500

    Updated file path with respect to GitHub

--- fileInput.py
+++ fileInput.py
@@ -10,7 +10,7 @@ Lexer = Lexer()
 def main():
     # Reads the file with the code
     a = []
-    with open("TestFiles/Test1.txt", encoding="utf-8") as f:
+    with open("Test1.txt", encoding="utf-8") as f:
         for line in f:
             for word in line.split():
                 a.append(word)
